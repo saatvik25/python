2f50260983b99f539f75ad9f51f5cb3d051ad457
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-07-05 12:06:59 +0530

    Create end.py

---
+++ end.py
@@ -0,0 +1,26 @@
+# What does end =’ do in Python
+
+
+# The end parameter in the print function is used to add any string. At the end of the output of the print statement in python.
+
+print("Toppr")
+
+print("is awesome")
+
+# Output:
+
+# Toppr is awesome
+
+print("hello", end = " ")
+print("python")
+
+# hello python
+
+# without end
+print("hello ")
+print("python")
+
+
+# hello 
+# python
+
