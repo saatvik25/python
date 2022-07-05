517d661140a3d381c00f53833caadbaf52ff6f5d
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-07-05 22:01:57 +0530

    polygon

---
+++ turtle/polygon.py
@@ -0,0 +1,14 @@
+
+
+
+#polyhon
+import turtle
+t = turtle.Turtle()
+t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+side =6
+side_length = 70
+side_angle = 360.0/side
+for i in range(6):
+  t.fd(side_angle)
+  t.right(side_length)
+t.mainloop()  
