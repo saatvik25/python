17cdc9a9bcd9d03998f31d0804d1314aba96ef79
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-07-06 08:16:55 +0530

    implement concurrent programming using lock

--- lock.py
+++ lock.py
@@ -14,7 +14,7 @@ def inc():
        
         x = x+1
         print(x)
-    # lock.release()
+    lock.release()
     print("lock is released ")  
     
 def dec():
@@ -34,3 +34,35 @@ t1 = threading.Thread(target = inc)
 t2 = threading.Thread(target = dec) 
 t1.start()
 t2.start()
+
+    
+    
+    
+#     lock is aquired 
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# lock is released 
+# lock is aquired 
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+# 0
+# lock is released 
+
+
+    
+        
